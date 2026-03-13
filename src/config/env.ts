f14b1d100fc3c684af05c53924ca81424f77c33a
ashrafulatif <ashrafulhaque08@gmail.com>
2026-03-14 04:20:06 +0600

    add: prisma + shcema

---
+++ src/config/env.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface EnvConfig {
+  NODE_ENV: string;
+  PORT: string;
+  DATABASE_URL: string;
+
+  ACCESS_TOKEN_SECRET: string;
+  ACCESS_TOKEN_EXPIRES_IN: string;
+
+  FRONTEND_URL: string;
+
+  CLOUDINARY: {
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  };
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requireEnvVariable = [
+    'NODE_ENV',
+    'PORT',
+    'DATABASE_URL',
+    'ACCESS_TOKEN_SECRET',
+    'ACCESS_TOKEN_EXPIRES_IN',
+    'FRONTEND_URL',
+    'CLOUDINARY_CLOUD_NAME',
+    'CLOUDINARY_API_KEY',
+    'CLOUDINARY_API_SECRET',
+  ];
+
+  requireEnvVariable.forEach((variable) => {
+    if (!process.env[variable]) {
+      throw new Error(
+        `Environment variable ${variable} is required but not set in .env file.`,
+      );
+    }
+  });
+
+  return {
+    NODE_ENV: process.env.NODE_ENV as string,
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+    CLOUDINARY: {
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+    },
+  };
+};
+
+export const envVars = loadEnvVariables();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { envVars } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envVars.PORT ?? 3000);
 }
 bootstrap();
